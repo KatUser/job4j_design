0a65490d0835083ba4942cb1767d1915edb8097c
KatUser <ekaterina.shirokikh@gmail.com>
2023-09-10 06:47:33 +0700

    7. Балансир   [#505083]

--- src/main/java/ru/job4j/iterator/Balancer.java
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class Balancer {
     public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
-        /* В итераторе source передют последовательность, nodes - списки для распределения */
         for (int i = 0; i <= nodes.size(); i++) {
             if (i == nodes.size()) {
                 i = 0;
