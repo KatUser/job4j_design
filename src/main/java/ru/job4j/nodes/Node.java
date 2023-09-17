063858565415a4cc6674007f26d2b1d7205c8294
KatUser <ekaterina.shirokikh@gmail.com>
2023-09-17 13:23:07 +0700

    Node example

---
+++ src/main/java/ru/job4j/nodes/Node.java
@@ -0,0 +1,69 @@
+package ru.job4j.nodes;
+
+public class Node<E> {
+    E item;
+    Node<E> next;
+    Node<E> prev;
+
+    transient int size = 0;
+    transient Node<E> first;
+    transient Node<E> last;
+    int modCount;
+
+    Node(Node<E> prev, E element, Node<E> next) {
+        this.item = element;
+        this.next = next;
+        this.prev = prev;
+    }
+    void linkLast(E e) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    void linkBefore(E e, Node<E> succ) {
+        final Node<E> pred = succ.prev;
+        final Node<E> newNode = new Node<>(pred, e, succ);
+        succ.prev = newNode;
+        if (pred == null) {
+            first = newNode;
+        } else {
+            pred.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    E unlink(Node<E> x) {
+        /* assert x != null; */
+        final E element = x.item;
+        final Node<E> next = x.next;
+        final Node<E> prev = x.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+        size--;
+        modCount++;
+        return element;
+    }
+}
