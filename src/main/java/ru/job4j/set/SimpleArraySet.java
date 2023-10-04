5a7855d36dec04613fa55d866d935f19b7cbcb54
KatUser <ekaterina.shirokikh@gmail.com>
2023-10-04 18:36:51 +0700

    1. Реализовать коллекцию Set на массиве [#996] draft

--- src/main/java/ru/job4j/set/SimpleArraySet.java
+++ src/main/java/ru/job4j/set/SimpleArraySet.java
@@ -3,6 +3,7 @@ package ru.job4j.set;
 import ru.job4j.collection.SimpleArrayList;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 public class SimpleArraySet<T> implements SimpleSet<T> {
     private SimpleArrayList<T> set = new SimpleArrayList<>(0);
@@ -21,7 +22,7 @@ public class SimpleArraySet<T> implements SimpleSet<T> {
     public boolean contains(T value) {
         boolean hasValue = false;
         for (int i = 0; i < set.size(); i++) {
-            if (set.get(i).equals(value)) {
+            if (Objects.equals(set.get(i), value)) {
                 hasValue = true;
                 break;
             }
---
+++ src/test/java/ru/job4j/set/SimpleArraySetTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.set;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.collection.SimpleArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleArraySetTest {
+
+    @Test
+    void whenAddNonNull() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.contains(1)).isTrue();
+        assertThat(set.add(1)).isFalse();
+    }
+
+    @Test
+    void whenAddSomeElementsNonNull() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.contains(1)).isFalse();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.contains(1)).isTrue();
+        assertThat(set.add(1)).isFalse();
+        assertThat(set.contains(2)).isFalse();
+        assertThat(set.add(2)).isTrue();
+        assertThat(set.contains(2)).isTrue();
+        assertThat(set.add(2)).isFalse();
+        assertThat(set.contains(3)).isFalse();
+        assertThat(set.add(3)).isTrue();
+        assertThat(set.contains(3)).isTrue();
+        assertThat(set.add(3)).isFalse();
+    }
+
+    @Test
+    void whenAddNull() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.add(null)).isTrue();
+        assertThat(set.contains(null)).isTrue();
+        assertThat(set.add(null)).isFalse();
+    }
+
+    @Test
+    void whenContainsWhatHasBeenAdded() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.add(0)).isTrue();
+        assertThat(set.contains(0)).isTrue();
+    }
+
+    @Test
+    void whenAddOneElementAndSizeIsOne() {
+        SimpleArrayList<Integer> set = new SimpleArrayList<>(0);
+        set.add(0);
+        assertThat(set.size()).isEqualTo(1);
+    }
+}
