cc0231aca0dbc5ed2e132d2bd003c6c9dbf3792a
KatUser <ekaterina.shirokikh@gmail.com>
2023-10-29 16:45:12 +0700

    3. Статистика по коллекции.   [#45889] shitty draft

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,31 +1,33 @@
 package ru.job4j.question;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
         Info info = new Info(0, 0, 0);
-        HashMap<Integer, String> previousMap = (HashMap<Integer, String>) previous.stream()
-                .collect(Collectors.toMap(User::getId, User::getName));
-        HashMap<Integer, String> currentMap = (HashMap<Integer, String>) current.stream()
-                .collect(Collectors.toMap(User::getId, User::getName));
+        Map<Integer, String> previousMap = new HashMap<>();
+        for (User user : previous) {
+            previousMap.put(user.getId(), user.getName());
+        }
 
-        for (Map.Entry<Integer, String> currEntry : currentMap.entrySet()) {
-            if (previousMap.containsKey(currEntry.getKey())
-                    && !Objects.equals(currEntry.getValue(), previousMap.get(currEntry.getKey()))) {
+        for (Map.Entry<Integer, String> prevEntry : previousMap.entrySet()) {
+            if (current.stream().anyMatch(c -> Objects.equals(c.getId(), prevEntry.getKey())
+                    && !Objects.equals(c.getName(), prevEntry.getValue()))) {
                 info.setChanged(info.getChanged() + 1);
             }
-            if (!previousMap.containsKey(currEntry.getKey())) {
-                info.setAdded(info.getAdded() + 1);
-            }
-        }
-        for (Map.Entry<Integer, String> prevEntry : previousMap.entrySet()) {
-            if (!currentMap.containsKey(prevEntry.getKey())) {
+            if (!current.stream().anyMatch(c -> Objects.equals(c.getId(), prevEntry.getKey()))) {
                 info.setDeleted(info.getDeleted() + 1);
             }
         }
+
+
+
+
         return info;
     }
 }
