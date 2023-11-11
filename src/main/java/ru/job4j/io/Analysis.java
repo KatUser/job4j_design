91df85c46951b440db234f75521f360a45de0059
KatUser <ekaterina.shirokikh@gmail.com>
2023-11-11 15:02:42 +0700

    7. Анализ доступности сервера [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -4,21 +4,36 @@ import java.io.*;
 
 public class Analysis {
     public void unavailable(String source, String target) {
+        boolean wroteTheStartOfDeadServer = false;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
             for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
                 String[] words = line.split(" ");
-                if (("400").equals(words[0]) || ("500").equals(words[0])) {
+                if ((("400").equals(words[0]) || ("500").equals(words[0])) && !wroteTheStartOfDeadServer) {
                     try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(
                             new FileOutputStream(target, true)))) {
                         printWriter.print(words[1] + ";");
                     } catch (FileNotFoundException e) {
                         throw new RuntimeException(e);
                     }
+                    wroteTheStartOfDeadServer = true;
+                }
+                if ((("200").equals(words[0]) || ("300").equals(words[0])) && wroteTheStartOfDeadServer) {
+                    try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(
+                            new FileOutputStream(target, true)))) {
+                        printWriter.println(words[1] + ";");
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                    wroteTheStartOfDeadServer = false;
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void writeTime() {
+
     }
 
 
