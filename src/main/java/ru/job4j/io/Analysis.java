329d5c672db9afad0f4f5c901217fc0dcf6ccf53
KatUser <ekaterina.shirokikh@gmail.com>
2023-11-11 15:47:09 +0700

    7. Анализ доступности сервера [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -7,23 +7,12 @@ public class Analysis {
         boolean wroteTheStartOfDeadServer = false;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
             for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
-                String[] words = line.split(" ");
-                if ((("400").equals(words[0]) || ("500").equals(words[0])) && !wroteTheStartOfDeadServer) {
-                    try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(
-                            new FileOutputStream(target, true)))) {
-                        printWriter.print(words[1] + ";");
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
+                if ((line.contains("400") || line.contains("500")) && !wroteTheStartOfDeadServer) {
+                    writeTime(target, line);
                     wroteTheStartOfDeadServer = true;
                 }
-                if ((("200").equals(words[0]) || ("300").equals(words[0])) && wroteTheStartOfDeadServer) {
-                    try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(
-                            new FileOutputStream(target, true)))) {
-                        printWriter.println(words[1] + ";");
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
+                if ((line.contains("200") || line.contains("300")) && wroteTheStartOfDeadServer) {
+                    writeTime(target, line);
                     wroteTheStartOfDeadServer = false;
                 }
             }
@@ -31,6 +20,21 @@ public class Analysis {
             e.printStackTrace();
         }
     }
+
+    public void writeTime(String target, String line) {
+        String[] lineSplit = line.split(" ");
+        try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(
+                new FileOutputStream(target, true)))) {
+            if (("400").equals(lineSplit[0]) || ("500").equals(lineSplit[0])) {
+                printWriter.print(lineSplit[1] + ";");
+            } else {
+                printWriter.println(lineSplit[1] + ";");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         Analysis analysis = new Analysis();
         analysis.unavailable("data/server.log", "data/target.csv");
