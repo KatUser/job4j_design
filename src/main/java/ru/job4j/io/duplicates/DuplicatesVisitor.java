da142bb4d32b2f12f59d85654f253079b6b7d2e3
KatUser <ekaterina.shirokikh@gmail.com>
2023-11-24 08:11:45 +0700

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -9,30 +9,28 @@ import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    public static Map<FileProperty, Path> pathMap = new HashMap<>();
-    public static Map<FileProperty, Path> duplicatesMap = new HashMap<>();
+    private static Map<FileProperty, Path> allFilesMap = new HashMap<>();
+    private static Map<FileProperty, Path> duplicatesMap = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) throws IOException {
         FileProperty fileProperty
                 = new FileProperty(file.toFile().length(), file.toFile().getName());
-        if (pathMap.containsKey(fileProperty)) {
+        if (allFilesMap.containsKey(fileProperty)) {
             duplicatesMap.put(fileProperty, file.toAbsolutePath());
 
         } else {
-            pathMap.put(fileProperty, file.toAbsolutePath());
+            allFilesMap.put(fileProperty, file.toAbsolutePath());
         }
         return super.visitFile(file, attributes);
     }
 
     public static void printFiles() {
-        FileProperty fp = null;
-        for (Map.Entry<FileProperty, Path> file : duplicatesMap.entrySet()) {
-            fp = file.getKey();
-            System.out.println(file.getValue());
-            for (Map.Entry<FileProperty, Path> filedupl : pathMap.entrySet()) {
-                if (filedupl.getKey().equals(fp)) {
-                    System.out.println(filedupl.getValue());
+        for (Map.Entry<FileProperty, Path> duplicateFile : duplicatesMap.entrySet()) {
+            for (Map.Entry<FileProperty, Path> file : allFilesMap.entrySet()) {
+                if (file.getKey().equals(duplicateFile.getKey())) {
+                    System.out.println(duplicateFile.getValue());
+                    System.out.println(file.getValue());
                 }
             }
         }
