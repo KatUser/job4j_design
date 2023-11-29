3b27a1a87df8eb91497f1087dece19808e0328ae
KatUser <ekaterina.shirokikh@gmail.com>
2023-11-29 18:15:12 +0700

    13. Именованные аргументы [#295518]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -11,43 +11,64 @@ public class ArgsName {
     public String get(String key) {
         String res = values.get("-" + key);
         if (res == null) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(String.format("This key: '%s' is missing", key));
         }
         return res;
     }
 
-    private void parse(String[] args) {
-        values.putAll(Arrays.stream(args)
+    private void parse(String[] arguments) {
+        values.putAll(Arrays.stream(arguments)
                         .map(String::trim)
                         .map(str -> str.split("=", 2))
                 .collect(Collectors.toMap(e -> e[0], e -> e[1])));
     }
 
-    public static ArgsName of(String[] args) {
-        checkArgsValidity(args);
+    public static ArgsName of(String[] arguments) {
+        checkArgsValidity(arguments);
         ArgsName names = new ArgsName();
-        names.parse(args);
+        names.parse(arguments);
+
         return names;
     }
 
-    public static void checkArgsValidity(String[] args) {
-        if (args.length < 2) {
+    public static void checkArgsValidity(String[] arguments) {
+        if (arguments.length < 1) {
             throw new IllegalArgumentException("Arguments not passed to program");
         }
-        if (!args[0].startsWith("-") || !args[1].startsWith("-")) {
-            throw new IllegalArgumentException("No hyphen");
+        for (String arg : arguments) {
+            if (!arg.startsWith("-")) {
+                throw new IllegalArgumentException(
+                        String.format("Error: This argument '%s' does not start with a '-' character", arg)
+                );
+            }
+            if (!arg.contains("=")) {
+                throw new IllegalArgumentException(
+                        String.format("Error: This argument '%s' does not contain an equal sign", arg)
+                );
+
+            }
+            if (arg.startsWith("-=")) {
+                throw new IllegalArgumentException(
+                        String.format("Error: This argument '%s' does not contain a key", arg)
+                );
+            }
+            if (arg.endsWith("=") && arg.chars().filter(ch -> ch == '=').count() < 2
+                    && arg.startsWith("-")) {
+                throw new IllegalArgumentException(
+                        String.format("Error: This argument '%s' does not contain a value", arg)
+                );
+            }
         }
-        if (!args[0].contains("=") || !args[1].contains("=")) {
-            throw new IllegalArgumentException("No equal");
-        }
-        if (args[0].startsWith("-=") || args[1].startsWith("-=")) {
-            throw new IllegalArgumentException("This argument has no key");
-        }
+
+
+
     }
 
 
     public static void main(String[] args) {
-        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
-        System.out.println(zip.get("encoding"));
+        String[] arguments = {"-Xmx=512", "-encoding=UTF-8"};
+        ArgsName jvm = ArgsName.of(arguments);
+        System.out.println(jvm.get("Xmx"));
+
     }
 }
