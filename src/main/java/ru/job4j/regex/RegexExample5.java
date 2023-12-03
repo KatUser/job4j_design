54a3c072534c6acb45898d6e717961c814cd2b54
KatUser <ekaterina.shirokikh@gmail.com>
2023-12-03 10:49:07 +0700

    RegexExample

---
+++ src/main/java/ru/job4j/regex/RegexExample5.java
@@ -0,0 +1,12 @@
+package ru.job4j.regex;
+
+import java.util.Arrays;
+
+public class RegexExample5 {
+    public static void main(String[] args) {
+        String str = "123+=-456:/789";
+        String[] rsl = str.split("\\D+");
+        System.out.println(Arrays.toString(rsl));
+
+    }
+}
