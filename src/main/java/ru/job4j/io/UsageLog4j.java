0c9e9e122e18cd87eadaee2863b6af59d4f9a612
KatUser <ekaterina.shirokikh@gmail.com>
2023-12-22 17:14:41 +0700

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,19 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String animal = "Cat";
+        char gender = 'F';
+        int paws = 4;
+        boolean isVaccinated = true;
+        short weightInKilos = 12;
+        long hairAmount = 922_330_000L;
+        byte foodPortionsPerDay = 3;
+        double foodPortionInGrams = 50.25855;
+        float sleepHoursPerWeek = 140.999F;
+        LOG.debug("Animal info : "
+                + " animal type : {}, male or female : {} , has vaccine : {}, number of paws : {} "
+                + " weights : {} kg, has {} hair, should eat a {}g portion at least {} per day "
+                + " and sleep {} hrs per week.", animal, gender, isVaccinated, paws, weightInKilos,
+                hairAmount, foodPortionInGrams, foodPortionsPerDay, sleepHoursPerWeek);
     }
 }
