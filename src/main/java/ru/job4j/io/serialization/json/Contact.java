31a34271ff54df7f7af20a57a3dff98413cf3cd3
KatUser <ekaterina.shirokikh@gmail.com>
2023-12-25 12:45:51 +0700

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/io/serialization/json/Contact.java
+++ src/main/java/ru/job4j/io/serialization/json/Contact.java
@@ -2,11 +2,12 @@ package ru.job4j.io.serialization.json;
 
 public class Contact {
     private final String phone;
-
     public Contact(String phone) {
         this.phone = phone;
     }
-
+    public String getPhone() {
+        return phone;
+    }
     @Override
     public String toString() {
         return "Contact{"
