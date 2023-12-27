70cdf165686041feb8a42c30e454870bd9067572
KatUser <ekaterina.shirokikh@gmail.com>
2023-12-27 07:07:21 +0700

    3. Поиск файлов по критерию [#783]

--- src/main/java/ru/job4j/io/filefinder/FileFinder.java
+++ src/main/java/ru/job4j/io/filefinder/FileFinder.java
@@ -2,46 +2,40 @@ package ru.job4j.io.filefinder;
 
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 import ru.job4j.io.ArgsName;
+import ru.job4j.io.SearchFiles;
 
 import java.io.FileFilter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 
-public class FileFinder extends SimpleFileVisitor<Path> {
+public class FileFinder {
     private static Path searchDirectory; /* директория, в которой начинать поиск.*/
     private static String match; /* имя файла, маска, либо регулярное выражение. */
     private static String searchType; /* mask искать по маске, name по полному совпадение имени,
     regex по регулярному выражению. */
     private static Path destinationFile; /* результат записать в файл. */
     private static Predicate<Path> condition;
-
-    @Override
-    public FileVisitResult visitFile(Path file,
-                                     BasicFileAttributes attributes) throws IOException {
-        if (condition.test(file)) {
-            logFile(file);
-        }
-        return super.visitFile(file, attributes);
+    public static void findFile() throws IOException {
+        condition = setCondition(match);
+        SearchFiles searchFiles = new SearchFiles(condition);
+        SearchFiles.search(destinationFile, condition);
+        Files.walkFileTree(searchDirectory, searchFiles);
+        logFile(searchFiles.getPaths());
     }
 
-    public static void findFile(Path path) throws IOException {
-        FileFinder fileFinder = new FileFinder();
-        setCondition(match);
-        Files.walkFileTree(searchDirectory, fileFinder);
-    }
-
-    public static void logFile(Path path) throws IOException {
+    public static void logFile(List<Path> paths) throws IOException {
         try (FileWriter fileWriter = new FileWriter(destinationFile.toFile(), true)) {
-            fileWriter.append(path.toString()).append(System.lineSeparator());
+            fileWriter.append(paths.toString()).append(System.lineSeparator());
         }
     }
 
-    private static void setCondition(String match) {
+    private static Predicate<Path> setCondition(String match) {
         if ("name".equals(searchType)) {
             condition = p -> p.toFile().getName().equals(match);
         }
@@ -53,6 +47,7 @@ public class FileFinder extends SimpleFileVisitor<Path> {
             Pattern pattern = Pattern.compile(match);
             condition = p -> pattern.matcher(p.getFileName().toString()).find();
         }
+        return condition;
     }
     /* -d=c:  -n=*.?xt -t=mask -o=log.txt*/
     private static void checkArgs(ArgsName argsName) {
@@ -91,6 +86,7 @@ public class FileFinder extends SimpleFileVisitor<Path> {
         match = argsName.get("n");
         searchType = argsName.get("t");
         destinationFile = Path.of(argsName.get("o"));
-        findFile(destinationFile);
+
+        findFile();
     }
 }
