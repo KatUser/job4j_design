949b3491c8db928d96fc6a2918a08e429d800a14
KatUser <ekaterina.shirokikh@gmail.com>
2024-01-08 12:09:47 +0700

    10. Операторы множества в SQL [#504896]

---
+++ db/2.3.4. Объекты Базы данных/10. Операторы множества в SQL [#504896]/multiple_selectors.sql
@@ -0,0 +1,102 @@
+CREATE TABLE customer
+(
+    first_name text,
+    last_name  text
+);
+
+CREATE TABLE employee
+(
+    first_name text,
+    last_name  text
+);
+
+INSERT INTO customer
+VALUES ('Иван', 'Иванов'),
+       ('Петр', 'Сергеев'),
+       ('Ирина', 'Бросова'),
+       ('Анна', 'Опушкина'),
+       ('Потап', 'Потапов');
+
+INSERT INTO employee
+VALUES ('Кристина', 'Позова'),
+       ('Михаил', 'Кругов'),
+       ('Анна', 'Опушкина'),
+       ('Иван', 'Иванов'),
+       ('Сергей', 'Петров');
+
+select first_name, last_name
+from customer
+union
+select first_name, last_name
+from employee;
+
+select first_name, last_name
+from customer
+union
+select first_name, last_name
+from employee
+order by first_name, last_name;
+
+select first_name, last_name
+from customer
+union all
+select first_name, last_name
+from employee
+order by first_name, last_name;
+
+select first_name, last_name
+from customer
+except
+select first_name, last_name
+from employee
+order by first_name, last_name;
+
+select first_name, last_name
+from customer
+intersect
+select first_name, last_name
+from employee
+order by first_name, last_name;
+
+CREATE TABLE referrer
+(
+    first_name text,
+    last_name  text
+);
+
+INSERT INTO referrer
+VALUES ('Евгений', 'Онегин'),
+       ('Петр', 'Сергеев'),
+       ('Александр', 'Ожегов'),
+       ('Анна', 'Опушкина'),
+       ('Михаил', 'Кругов');
+
+SELECT first_name, last_name
+FROM customer
+UNION
+SELECT first_name, last_name
+FROM employee
+UNION
+SELECT first_name, last_name
+FROM referrer
+ORDER BY first_name, last_name;
+
+SELECT first_name, last_name
+FROM customer
+UNION ALL
+SELECT first_name, last_name
+FROM employee
+EXCEPT
+SELECT first_name, last_name
+FROM referrer
+ORDER BY first_name, last_name;
+
+SELECT first_name, last_name
+FROM customer
+UNION ALL
+(SELECT first_name, last_name
+ FROM employee
+ EXCEPT
+ SELECT first_name, last_name
+ FROM referrer)
+ORDER BY first_name, last_name;
